1b10aa2960076466da909d6a2572d133049d8bd6
asanjose <Aaron@Aarons-MacBook-Pro.local>
2018-05-19 18:54:51 -0400

    RideAlongAPI

--- RideAlongAPI/RideAlongAPI/Controllers/ValuesController.cs
+++ RideAlongAPI/RideAlongAPI/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace RideAlongAPI.Controllers
     [Authorize]
     public class ValuesController : ApiController
     {
-        //testing webhook
+        //testing 
         // GET api/values
         public IEnumerable<string> Get()
         {
