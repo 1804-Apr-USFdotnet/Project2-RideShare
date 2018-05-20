9a289c9379e4c58afad136b05d7f1615f3461e79
asanjose <Aaron@Aarons-MacBook-Pro.local>
2018-05-20 16:39:56 -0400

    RideAlongMVC

--- RideAlongMVC/RideAlongMVC/Controllers/HomeController.cs
+++ RideAlongMVC/RideAlongMVC/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace RideAlongMVC.Controllers
 {
-    //testing webhook
+    //testing webhookk
     public class HomeController : Controller
     {
         public ActionResult Index()
