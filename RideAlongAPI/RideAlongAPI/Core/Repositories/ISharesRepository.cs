e787a0d872b30f7116aaf9c6365a1d6668061d4b
asanjose <Aaron@USF-Gold-Wifi-226-110-73.laptops.usf.edu>
2018-05-21 16:19:15 -0400

    RideAlongAPI

--- RideAlongAPI/RideAlongAPI/Core/Repositories/ISharesRepository.cs
+++ RideAlongAPI/RideAlongAPI/Core/Repositories/ISharesRepository.cs
@@ -6,6 +6,9 @@ namespace RideAlongAPI.Core.Repositories
 {
     public interface ISharesRepository : IRepository<Share>
     {
+        IEnumerable<Share> GetDesiredShare(string startcity, string goalcity);
+        IEnumerable<Share> GetSeatsDescending();
+        IEnumerable<Share> GetDateDescending();
 
     }
 }
