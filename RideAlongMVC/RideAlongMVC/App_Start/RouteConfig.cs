5de8dc90b3799f0a2bbd108282ced78b093321c8
Chase Taylor <tayl1776@gmail.com>
2018-05-21 15:43:54 -0400

    changed default route

--- RideAlongMVC/RideAlongMVC/App_Start/RouteConfig.cs
+++ RideAlongMVC/RideAlongMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RideAlongMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "", action = "", id = UrlParameter.Optional }
             );
         }
     }
