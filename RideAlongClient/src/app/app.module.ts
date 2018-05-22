128d4bc72603396bce6624c773ff5c357101562f
newlandp <paulnewland@hotmail.com>
2018-05-22 11:03:53 -0400

    add home and about components and links in the nav to them in angular app

--- RideAlongClient/src/app/app.module.ts
+++ RideAlongClient/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
