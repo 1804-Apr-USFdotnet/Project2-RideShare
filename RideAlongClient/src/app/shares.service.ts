3e9092a4c726367b761f6a08353cb3a07ae43cd6
newlandp <paulnewland@hotmail.com>
2018-05-23 14:51:24 -0400

    set up structure to get all shares http request and log to console

--- RideAlongClient/src/app/shares.service.ts
+++ RideAlongClient/src/app/shares.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharesService {
+  private readonly rootUrl: string = 'url';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllShares() {
+    return this.http.get(this.rootUrl);
+  }
 }
--- RideAlongClient/src/app/shares/shares.component.ts
+++ RideAlongClient/src/app/shares/shares.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharesService } from '../shares.service';
 
 @Component({
   selector: 'app-shares',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shares.component.css']
 })
 export class SharesComponent implements OnInit {
+  shares: any[];
 
-  constructor() { }
+  constructor(private sharesService: SharesService) { }
 
   ngOnInit() {
+    this.sharesService.getAllShares().subscribe(
+      (response: Response) => console.log(response.json()),
+      (error) => console.log(error)
+    )
   }
-
 }
