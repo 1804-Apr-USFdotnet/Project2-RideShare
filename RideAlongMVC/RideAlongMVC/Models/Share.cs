a784493d92ea93af4b93deae9d564351e0026358
Chase Taylor <tayl1776@gmail.com>
2018-05-24 11:54:33 -0400

    began implementing mvc

---
+++ RideAlongMVC/RideAlongMVC/Models/Share.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RideAlongMVC.Models
+{
+    public class Share
+    {
+        public int Id { get; set; }
+        public int Seats { get; set; }
+        public string DestinationCity { get; set; }
+        public string DepartureCity { get; set; }
+        public DateTime DepartureDate { get; set; }
+        //public User Driver { get; set; }
+        //public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ RideAlongMVC/RideAlongMVC/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RideAlongMVC.Models
+{
+    public class User
+    {
+    }
+}
