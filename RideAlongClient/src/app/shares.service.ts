a74c71a577c98ac02cd31d29f637ae3a97a51e37
asanjose <Aaron@Aarons-MacBook-Pro.local>
2018-05-26 16:34:27 -0400

    Added some services

--- RideAlongClient/src/app/shares.service.ts
+++ RideAlongClient/src/app/shares.service.ts
@@ -6,6 +6,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SharesService {
   private readonly rootUrl: string = 'http://localhost:50235/api/shares';
+  private readonly custDateUrl: string = "http://localhost:50235/api/Shares-ByDate-Descending";
+  private readonly custSeatUrl: string = "http://localhost:50235/api/Shares-Seat-Descending";
+  private readonly custDepartUrl: string = "http://localhost:50235/api/Shares-Most-Departing-City";
+  private readonly custDestUrl: string = "http://localhost:50235/api/Shares-Most-Destination-City";
+  private readonly SearchUrl: string = "http://localhost:50235/api/Shares-Search-Conditions-City/";
+  private readonly SetUpUrl: string = "http://localhost:50235/api/Shares-Setup-Ride/";
 
   constructor(private http: HttpClient) { }
 
@@ -28,4 +34,30 @@ export class SharesService {
   deleteShare(id: number) {
     return this.http.delete(`${this.rootUrl}/${id}`);
   }
+
+  DescendingSeatShare(){
+    return this.http.get(this.custSeatUrl);
+  }
+
+  DescendingDateShare(){
+    return this.http.get(this.custDateUrl);
+  }
+
+  MostDepartingCity(){
+    return this.http.get(this.custDepartUrl);
+  }
+
+  MostDestCity(){
+    return this.http.get(this.custDestUrl);
+  }
+
+  SearchFor(desiredText: string){
+    return this.http.get(`${this.SearchUrl}/${desiredText}`);
+  }
+
+  SetUpRide(departingCity: string, arrivingCity: string ){
+    return this.http.get(`${this.SetUpUrl}/${departingCity}/${arrivingCity}`);
+  }
+
+
 }
--- RideAlongClient/src/app/shares/shares.component.ts
+++ RideAlongClient/src/app/shares/shares.component.ts
@@ -20,4 +20,44 @@ export class SharesComponent implements OnInit {
       (error) => console.log(error)
     )
   }
+
+  SortSeat(){
+    this.sharesService.DescendingSeatShare().subscribe(
+      (response: any[]) => {
+        this.shares = response;
+        console.log(response);
+      },
+      (error) => console.log(error)
+    )
+  }
+
+  SortDate(){
+    this.sharesService.DescendingDateShare().subscribe(
+      (response: any[]) => {
+        this.shares = response;
+        console.log(response);
+      },
+      (error) => console.log(error)
+    )
+  }
+
+  MostDepartingCity(){
+    this.sharesService.MostDepartingCity().subscribe(
+      (response: any[]) => {
+        this.shares = response;
+        console.log(response);
+      },
+      (error) => console.log(error)
+    )
+  }
+
+  MostDestinationCity(){
+    this.sharesService.MostDestCity().subscribe(
+      (response: any[]) => {
+        this.shares = response;
+        console.log(response);
+      },
+      (error) => console.log(error)
+    )
+  }
 }
